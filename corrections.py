43e749d91e131e6c6f9476447403e952e01af8b7
mgbarsky <mgbarsky@gmail.com>
2020-09-10 11:42:58 -0400

    more corrections

--- corrections.py
+++ corrections.py
@@ -6,8 +6,8 @@ def random_optimize(domain,  fitness_function, n=9999):
     for iter in range(n):
         # Create a random solution
         # domain[i] represents a range of all valid choices for position i
-        r=[random.choice(domain[i])
-                      for i in range(len(domain))]
+        r=[random.randint(domain[i][0],domain[i][1])
+                      for i in range(len(domain))]        
     
         # Get the cost
         cost=fitness_function(r)
@@ -23,7 +23,7 @@ def random_optimize(domain,  fitness_function, n=9999):
 # this code goes into code cell 1.2
 def hillclimb_optimize(domain, fitness_function):
     # Create an initial random solution
-    best_sol=[random.choice(domain[i])
+    best_sol=[random.randint(domain[i][0],domain[i][1])
               for i in range(len(domain))]
 
     # Main loop: until no better neighbors
@@ -45,7 +45,7 @@ def hillclimb_optimize(domain, fitness_function):
                 neighbors.append(best_sol[0:j]+[best_sol[j]-1]+best_sol[j+1:])
             # if current best-SOl[j] is not the largest in the domain 
             # - we can add 1 
-            if best_sol[j]< domain[j][1] - 1:
+            if best_sol[j]< domain[j][1]:
                 neighbors.append(best_sol[0:j]+[best_sol[j]+1]+best_sol[j+1:])
 
         # See what the best solution amongst the neighbors is        
@@ -69,7 +69,7 @@ def hillclimb_optimize(domain, fitness_function):
 def annealing_optimize(domain, fitness_function,
                       T=10000.0,cool=0.95,step=1):
     # Create an initial random solution
-    best_sol =[random.choice(domain[i])
+    best_sol =[random.randint(domain[i][0],domain[i][1])
               for i in range(len(domain))]
     
     iterations = 0
@@ -93,8 +93,8 @@ def annealing_optimize(domain, fitness_function,
             sol_b [i] = domain[i][0]
         # if sol_b[i] larger than the max allowed value
         # replace it with the largest from the domain
-        elif sol_b [i] > domain[i][1]-1:
-            sol_b [i] = domain[i][1] - 1
+        elif sol_b [i] > domain[i][1]:
+            sol_b [i] = domain[i][1] 
 
         # Calculate the cost of the current_best solution
         best_score = fitness_function(best_sol)
@@ -134,7 +134,7 @@ def genetic_optimize (domain, fitness_function,
         # either increase or decrease value at i by step
         if random.random()<0.5 and (vec[i] - step) >= domain[i][0]:
             return vec[0:i]+[vec[i]-step]+vec[i+1:]
-        elif (vec[i]+step) < domain[i][1]:
+        elif (vec[i]+step) <= domain[i][1]:
             return vec[0:i]+[vec[i]+step]+vec[i+1:]
         return vec
 
@@ -146,7 +146,7 @@ def genetic_optimize (domain, fitness_function,
     # Build the initial population of random solutions
     pop=[]
     for i in range(popsize):
-        vec = [random.choice(domain[i])
+        vec = [random.randint(domain[i][0],domain[i][1])
                for i in range(len(domain))]
         pop.append(vec)
 
@@ -188,4 +188,4 @@ def genetic_optimize (domain, fitness_function,
                 pop.append(crossover(ranked_solutions[c1],ranked_solutions[c2]))
                 
     # After n generations return the top scored solution from the sorted list of scores
-    return (scores[0][1], scores[0][0], generations) 
+    return (scores[0][1], scores[0][0], generations)
